e8e6acc8e7955a9a16519a7af4675d60a8427d48
Eirikur <eirikurb19@ru.is>
2021-03-01 19:28:07 +0000

    Plexi Implementation

--- Autoclicker.py
+++ Autoclicker.py
@@ -8,7 +8,7 @@ x = 1366
 y = 768
 
 # seconds between each iteration
-time_interval = 10
+time_interval = 3
 
 # color white
 white = [255, 255, 255]     
@@ -18,7 +18,8 @@ while True:
     screen = np.array(ImageGrab.grab(bbox=[x, y, x+1, y+1]))[0][0] # use indices to get only one pixel
 
     # check if the pixel is white
-    if screen != white:
+    if screen[0] != 255:
+        print('not white')
 
         # move mouse to pixel coordinates
         mouse.move(x,y, absolute=True, duration=0)
@@ -27,4 +28,6 @@ while True:
         mouse.click('left')
 
     # sleep for x amount of time
+    else:
+        print('white')
     sleep(time_interval)
